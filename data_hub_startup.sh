565b1286539c6af73b550e429b767ecca6809fcd
LukaszS <l.sobocinski123@gmail.com>
2022-08-12 18:16:46 +0200

    work

--- data_hub_startup.sh
+++ data_hub_startup.sh
@@ -1,3 +1,4 @@
 sudo ip link set can0 type can bitrate 250000
 sudo ip link set can0 up
-python3 run.py #send-board-computer=False send-cloud=True log-path=logs.log backup-path=backup.bin
+docker compose up
+python3 run.py
