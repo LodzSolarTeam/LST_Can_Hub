e006cc9517db6a47c136e128b8bc88072738b76f
LukaszS <l.sobocinski123@gmail.com>
2022-08-13 13:49:36 +0200

    works

--- receivers/therm_sensors.py
+++ receivers/therm_sensors.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import time
 import struct
 
 from w1thermsensor import W1ThermSensor
@@ -7,18 +8,20 @@ from w1thermsensor import W1ThermSensor
 from car import Car
 
 async def motor_temperature_receiver(car: Car):
+    loop = asyncio.get_event_loop()
     while True:
         try:
             sensors = W1ThermSensor.get_available_sensors()
-            logging.info(f"[Motor Temperature Receiver] {sensors}")
+            logging.debug(f"[Motor Temperature Receiver] {sensors}")
             for sensor in sensors:
                 temperature = int(
                     float(
                         round(sensor.get_temperature(), 2)
                     ) * 100
                 )
-                car.General.motorTemperatureArray[:] = struct.pack('<H', temperature)
-            await asyncio.sleep(0.7)
+                
+            # car.General.motorTemperatureArray[:] = struct.pack('<H', temperature)
+            await loop.run_in_executor(None, time.sleep, 1)
         except Exception as e:
-            print(str(e))
+            logging.warning(str(e))
 
