f562482e0a8b722e87c09ec58ed6c7e00b82b3b1
Piotr Krupa <piotrkrupa06@gmail.com>
2022-11-02 20:04:08 +0100

    Improve sending

--- cloud_sender.py
+++ cloud_sender.py
@@ -14,43 +14,34 @@ class CloudSender:
         self.queue = SQLiteAckQueue('./car_queue')
         self.mqtt_client = Client()
         self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_publish = self.on_publish
 
         self.current_message = None
 
-        logging.info("inited")
+        logging.info("Initialized")
 
     def run(self):
-        # self._run_queue()
         self.mqtt_client.connect(host, keepalive=True)
         self.mqtt_client.loop_start()
 
         while True:
-            logging.info("Waiting for message")
-            self.current_message = self.queue.get()
-            logging.info(f"get {self.current_message}")
+            try:
+                self.current_message = self.queue.get()
+                logging.info(f"get {self.current_message}")
 
-            # qos=1 means that the broker will make sure that the message is delivered
-            info = self.mqtt_client.publish("eagle2", self.current_message)
-            info.wait_for_publish()
+                info = self.mqtt_client.publish("eagle2", self.current_message)
+                info.wait_for_publish()
+
+                self.queue.ack(self.current_message)
+            except Exception as e:
+                logging.error(f"Error: {e}")
+                self.queue.nack(self.current_message)
+
+            self.current_message = None
 
         self.mqtt_client.loop_stop()
 
-
     def on_connect(self, client, userdata, flags, rc):
-
         logging.info("Connected with result code " + str(rc))
-
-
-    def on_publish(self, client, userdata, mid):
-        logging.info(f"Published `{self.current_message}` with mid `{mid}` and userdata `{userdata}`")
-
-        if userdata == None:
-            logging.info("Message published")
-            self.queue.ack(self.current_message)
-        else:
-            logging.info("Message not published")
-            self.queue.nack(self.current_message)
 
 async def main():
     cloud_sender = CloudSender()
